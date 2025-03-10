e6a34f2cbccc40436cbb676db06e770bc7506352
Joop Ringelberg <joop.ringelberg@gmail.com>
2025-03-10 13:02:40 +0100

    We have switched to typescript and rollup.

--- src/languages/perspectives-arc.ts
+++ src/languages/perspectives-arc.ts
@@ -1,4 +1,4 @@
-import { LanguageFn } from 'highlight.js';
+import { LanguageFn, HLJSApi, Mode, Language } from 'highlight.js';
 
 /*
 Language: Perspectives ARC
@@ -27,25 +27,24 @@ const
     base0F = "meta"
     ;
 
-const whitespace = /\s+/;
+const whitespace: RegExp = /\s+/;
 
-const identifier = /\w+/;
+const identifier: RegExp = /\w+/;
 
-function lexeme(w : string)
-{
-    return new RegExp( "\\b" + w + "\\b");
+function lexeme(w: string): RegExp {
+    return new RegExp("\\b" + w + "\\b");
 }
 const lex = lexeme;
 const l = lexeme;
 
 // CONTEXT KINDS (base0A)
-const contexts = {
+const contexts: Mode = {
     scope: base0A,
     match: /\b(domain|case|party|activity)\b/,
 };
 
 // ROLE KINDS (NOT USER) (base0B)
-const roles ={
+const roles: Mode = {
     begin: /(\n\s*)\b(external|thing|context)\b/,
     beginScope: {
         2: base08
@@ -53,48 +52,47 @@ const roles ={
 };
 
 // USER ROLE IS SPECIAL (role name gets base03)
-const user = {
+const user: Mode = {
     begin: [lex("user"), whitespace, identifier],
     beginScope: {
         1: base0B,
         3: base03
     }
-
 };
 
-const property = {
+const property: Mode = {
     scope: base08,
     begin: lexeme("property")
 };
 
-const propertyFacets = {
+const propertyFacets: Mode = {
     scope: base08,
     begin: /\b(mandatory|relational|unlinked|minLength|maxLength|enumeration|pattern|maxInclusive|minInclusive)\b/
 };
 
-const perspective = {
+const perspective: Mode = {
     scope: base0D,
     begin: /\bperspective(\s+(on|of))?\b/,
 }
 
-const perspectiveParts = {
+const perspectiveParts: Mode = {
     scope: base0D,
     begin: /\b(view(?=\s+[\w|:|\$]+\s+verbs)|verbs|props|only|except|defaults|all\s+roleverbs|action)\b/,
 }
 
-const state = {
+const state: Mode = {
     scope: base0E,
     begin: lexeme("state")
 };
 
 // STATE TRANSITION (ON ENTRY, ON EXIT)
-const stateTransition = {
+const stateTransition: Mode = {
     scope: base0E,
     begin: /\bon\s+(entry|exit)\b/
 };
 
 // NOTIFICATION
-const notification = {
+const notification: Mode = {
     begin: [lexeme("notify"), whitespace, identifier],
     beginScope: {
         1: base0E,
@@ -104,7 +102,7 @@ const notification = {
 
 // AUTOMATIC ACTION
 // User in `do for user` gets the role coloring.
-const automaticAction = {
+const automaticAction: Mode = {
     begin: [lexeme("do"), whitespace, lexeme("for"), whitespace, identifier],
     beginScope: {
         1: base0E,
@@ -113,13 +111,13 @@ const automaticAction = {
     }
 };
 
-const timing = {
+const timing: Mode = {
     scope: base0E,
     begin: /\b(after|until|every|maximally|times)\b/
 }
 
 // ASSIGNMENT
-const remove = {
+const remove: Mode = {
     begin: [lexeme("remove"), whitespace, /\b(role|context)\b/],
     beginScope: {
         1: base0E,
@@ -127,8 +125,8 @@ const remove = {
     }
 };
 
-const create = {
-    begin: [lexeme( "create" ), whitespace, lexeme("role"), whitespace, identifier, whitespace, lexeme("in")],
+const create: Mode = {
+    begin: [lexeme("create"), whitespace, lexeme("role"), whitespace, identifier, whitespace, lexeme("in")],
     beginScope: {
         1: base0E,
         3: base0E,
@@ -136,8 +134,8 @@ const create = {
     }
 };
 
-const deleteStatement = {
-    begin: [lexeme( "delete" ), whitespace, lexeme("role"), whitespace, identifier, whitespace, lexeme("from")],
+const deleteStatement: Mode = {
+    begin: [lexeme("delete"), whitespace, lexeme("role"), whitespace, identifier, whitespace, lexeme("from")],
     beginScope: {
         1: base0E,
         3: base0E,
@@ -146,38 +144,38 @@ const deleteStatement = {
 };
 
 // SIMPLE VALUES
-const boolean = {
+const boolean: Mode = {
     begin: /true|false/,
     scope: base09
 };
 
-const date = {
+const date: Mode = {
     begin: /\'[\d|\-]+\'/,
     scope: base09
 };
 
-const number = {
+const number: Mode = {
     begin: /\b\d+\b/,
     scope: base09
 };
 
-const regexp = {
+const regexp: Mode = {
     begin: /\/.*\//,
     scope: base09
 };
 
-const timeConstants = {
+const timeConstants: Mode = {
     begin: /Milliseconds|Seconds|Minutes|Hours|Days/,
     scope: base09
 }
 
 // OPERATORS
-const alphabeticOperator = {
+const alphabeticOperator: Mode = {
     begin: /\b(either|both|binds|matches|and|or|not|exists|available|boundBy|binder|context|extern)\b/,
     scope: base0C
 };
 
-const filter = {
+const filter: Mode = {
     begin: [lexeme("filter"), /.*/, lexeme("with")],
     beginScope: {
         1: base0C,
@@ -185,42 +183,42 @@ const filter = {
     }
 };
 
-const nonAlphabeticOperator = {
+const nonAlphabeticOperator: Mode = {
     begin: /\>\>\=|\>\>|\*|\/|\+|\-|\=\=|\>\=|\<|>\=|>/,
     scope: base0C
 };
 
 // STANDARD VARIABLES
-const standardVariables = {
+const standardVariables: Mode = {
     begin: /\b(currentcontext|nofifieduser|origin|currentactor)\b/,
     scope: base03
 };
 
 // PROPERTY RANGE
-const propertyRange = {
+const propertyRange: Mode = {
     begin: /\b(String|Number|Boolean|DateTime)\b/,
     scope: base09
 };
 
 // ROLE VERBS
-const roleVerbs = {
+const roleVerbs: Mode = {
     begin: /\b(Remove|Delete|Create|CreateAndFill|Fill|Unbind|RemoveFiller|Move)\b/,
     scope: base0F
 };
 
 // PROPERTY VERBS
-const propertyVerbs = {
+const propertyVerbs: Mode = {
     begin: /\b(RemovePropertyValue|DeleteProperty|AddPropertyValue|SetPropertyValue|Consult)\b/,
     scope: base0F
 };
 
 // META: ASPECT, INDEXED
-const meta = {
+const meta: Mode = {
     begin: / \b(aspect|indexed)\b/,
     scope: base05
 };
 
-const use = {
+const use: Mode = {
     begin: [lexeme("use"), /.*/, lexeme("for")],
     beginScope: {
         1: base05,
@@ -228,13 +226,13 @@ const use = {
     }
 };
 
-const blockComment = {
+const blockComment: Mode = {
     scope: base03,
     begin: /\{-/,
     end: /-\}/
 };
 
-const perspectivesArc: LanguageFn = function(hljs) {
+const perspectivesArc: LanguageFn = function(hljs: HLJSApi): Language {
     // COMMENT
     const simpleComment = hljs.COMMENT('--', '$');
 
